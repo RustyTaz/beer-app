65b93d81d238b7abd6e0bf4a5a871599cf6bc958
Rustam Tazyaev <rustytazyaev@gmail.com>
2023-04-12 14:23:29 +0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'beer.app',
+  appName: 'beer-app',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
